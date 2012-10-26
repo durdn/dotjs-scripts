1b4717c3b144d7061a91aa2c7b2d0a923c3d0a67
Nicola Paolucci <nick@durdn.com>
2012-10-26 20:13:08 +0200

    pathe.nl: use local storage to cache results

--- pathe.nl.js
+++ pathe.nl.js
@@ -1,20 +1,29 @@
 $('.movie-highlight .heading a').each(function(index, movie) { 
   var title = $(movie).text();
-  var query = 'http://www.rottentomatoes.com/search/?search='+encodeURIComponent(title)+'&sitesearch=rt';
-  $.get(query, function(data) {
-    //var rating = $('#movie_results_ul .tmeter', data).text();
-    var rating = $('#all-critics-numbers .tomato_numbers #all-critics-meter', data).text();
-    if (rating == '') {
-      //console.log('trying next method' + query);
-      rating = $('#movie_results_ul li', data).first().find('.tMeterScore').text();
-      if (rating != '') {
-        //console.log(title + ' found ' + rating);
+  var hasRating = localStorage.getItem(title);
+  if (hasRating) {
+      console.log('using local storage:' + hasRating);
+      $(movie).append($('<p />').text('rotten: ' + hasRating));
+  } else {
+    var query = 'http://www.rottentomatoes.com/search/?search='+encodeURIComponent(title)+'&sitesearch=rt';
+    $.get(query, function(data) {
+      //var rating = $('#movie_results_ul .tmeter', data).text();
+      var rating = $('#all-critics-numbers .tomato_numbers #all-critics-meter', data).text();
+      if (rating == '') {
+        //console.log('trying next method' + query);
+        rating = $('#movie_results_ul li', data).first().find('.tMeterScore').text();
+        if (rating != '') {
+          //console.log(title + ' found ' + rating);
+        } else {
+          rating = 'not found';
+        }
       } else {
-        rating = 'not found';
+        rating = rating + '%';
       }
-    } else {
-      rating = rating + '%';
-    }
-    $(movie).append($('<p />').text('rotten: ' + rating));
-  });
+      if(typeof(Storage)!=="undefined") {
+        localStorage.setItem(title, rating);
+      }
+      $(movie).append($('<p />').text('rotten: ' + rating));
+    });
+  }
 });
