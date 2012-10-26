b920f9bd27a3c28551d0d77edeb859b2d505621e
Nicola Paolucci <nick@durdn.com>
2012-10-26 23:33:21 +0200

    pathe.nl: added metacritic rating inject

--- pathe.nl.js
+++ pathe.nl.js
@@ -12,30 +12,54 @@ function getRottenRating(title, callback) {
   });
 }
 
-function injectRating(movie, rating) {
-  if (rating !== '') {
-    $(movie).append($('<p />').text('rotten: ' + rating));
-  }
+function getMetacriticRating(title, callback) {
+  var rating = '';
+  title = title.toLowerCase().replace(/ /g, '+');
+  var query = 'http://www.metacritic.com/search/all/'+encodeURIComponent(title)+'/results';
+  console.log('meta url:' + query);
+  $.get(query, function(data) {
+    var rating = $('.search_results .first_result .metascore', data).text();
+    callback(rating);
+  });
 }
 
-$('.movie-highlight .heading a').each(function(index, movie) { 
-  var title = $(movie).text();
-  var rating = localStorage.getItem(title);
-  if (rating) {
-      console.log('using local storage:' + rating);
-      injectRating(movie, rating);
-  } else {
-      getRottenRating(title, function(ajaxRating) {
-        console.log('queried rotten:' + ajaxRating);
-        injectRating(movie,ajaxRating);
-        localStorage.setItem(title, ajaxRating);
-      });
+function injectRating(site, movie, rating) {
+  if (rating !== '') {
+    $(movie).append($('<p />').text(site + ' ' + rating));
   }
-});
+}
 
 $('span:contains("nederlands")').css('color','red');
 $('span:contains("nederlands")').parents('.movie-highlight').next('.day-table').remove();
 $('span:contains("nederlands")').parents('.movie-highlight').remove();
 $('span:contains("turkse")').parents('.movie-highlight').next('.day-table').remove();
 $('span:contains("turkse")').parents('.movie-highlight').remove();
+
+$('.movie-highlight .heading a').each(function(index, movie) { 
+  var title = $(movie).text();
+
+  var rating = localStorage.getItem(title);
+  if (rating) {
+      console.log('using local storage:' + rating);
+      injectRating('rotten:', movie, rating);
+  } else {
+      getRottenRating(title, function(ajaxRating) {
+        console.log('queried rotten:' + ajaxRating);
+        injectRating('rotten:', movie,ajaxRating);
+        localStorage.setItem(title, ajaxRating);
+      });
+  }
+
+  var metarating = localStorage.getItem('metacritic-' + title);
+  if (metarating) {
+      console.log('using local storage:' + metarating);
+      injectRating('metacritic:', movie, metarating);
+  } else {
+      getMetacriticRating(title, function(ajaxRating) {
+        console.log('queried metacritic:' + ajaxRating);
+        injectRating('metacritic:', movie, ajaxRating);
+        localStorage.setItem('metacritic-' + title, ajaxRating);
+      });
+  }
+});
 
