054195fc4d4e0cc31c11e8df06f7557b3f8745ae
Nicola Paolucci <nick@durdn.com>
2012-10-26 20:45:07 +0200

    pathe.nl: refactored and cleaned

--- pathe.nl.js
+++ pathe.nl.js
@@ -1,29 +1,34 @@
+function getRottenRating(title, callback) {
+  var rating = '';
+  var query = 'http://www.rottentomatoes.com/search/?search='+encodeURIComponent(title)+'&sitesearch=rt';
+  $.get(query, function(data) {
+    var rating = $('#all-critics-numbers .tomato_numbers #all-critics-meter', data).text();
+    if (rating == '') {
+      rating = $('#movie_results_ul li', data).first().find('.tMeterScore').text();
+    } else {
+      rating = rating + '%';
+    }
+    callback(rating);
+  });
+}
+
+function injectRating(movie, rating) {
+  if (rating !== '') {
+    $(movie).append($('<p />').text('rotten: ' + rating));
+  }
+}
+
 $('.movie-highlight .heading a').each(function(index, movie) { 
   var title = $(movie).text();
-  var hasRating = localStorage.getItem(title);
-  if (hasRating) {
-      console.log('using local storage:' + hasRating);
-      $(movie).append($('<p />').text('rotten: ' + hasRating));
+  var rating = localStorage.getItem(title);
+  if (rating) {
+      console.log('using local storage:' + rating);
+      injectRating(movie, rating);
   } else {
-    var query = 'http://www.rottentomatoes.com/search/?search='+encodeURIComponent(title)+'&sitesearch=rt';
-    $.get(query, function(data) {
-      //var rating = $('#movie_results_ul .tmeter', data).text();
-      var rating = $('#all-critics-numbers .tomato_numbers #all-critics-meter', data).text();
-      if (rating == '') {
-        //console.log('trying next method' + query);
-        rating = $('#movie_results_ul li', data).first().find('.tMeterScore').text();
-        if (rating != '') {
-          //console.log(title + ' found ' + rating);
-        } else {
-          rating = 'not found';
-        }
-      } else {
-        rating = rating + '%';
-      }
-      if(typeof(Storage)!=="undefined") {
-        localStorage.setItem(title, rating);
-      }
-      $(movie).append($('<p />').text('rotten: ' + rating));
-    });
+      getRottenRating(title, function(ajaxRating) {
+        console.log('queried rotten:' + ajaxRating);
+        injectRating(movie,ajaxRating);
+        localStorage.setItem(title, ajaxRating);
+      });
   }
 });
